4324e894457f0a65392274d5d5572cbe2b21048e
jesusandrespitrecerra-rgb <jesusandrespitrecerra@gmail.com>
2026-09-17 17:39:53 -0500

    Add files via upload

---
+++ src/app/index.tsx
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Asisencia</Text>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#11c1c1",
+  },
+  text: {
+    color: "#fff",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+});
