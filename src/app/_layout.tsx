817ee44d6a511e45bd06ab9f1297516614d70ea5
jesusandrespitrecerra-rgb <jesusandrespitrecerra@gmail.com>
2026-09-17 18:59:34 -0500

    Replace AppTabs with Stack navigation
    modifique el codigo

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,4 +1,4 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
+import { DarkTheme, DefaultTheme, Stack, ThemeProvider } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useColorScheme } from 'react-native';
 
@@ -11,8 +11,10 @@ export default function TabLayout() {
   const colorScheme = useColorScheme();
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
+      <Stack>
+        <Stack.Screen name="index" options={{title: "Home"}} />
+        <Stack.Screen name="about" options={{title: "About Us"}} />
+      </Stack>
     </ThemeProvider>
   );
 }
