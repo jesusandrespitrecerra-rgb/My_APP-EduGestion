9255c196bd47b447fd7e7f798b612800aeed8f5c
jesusandrespitrecerra-rgb <jesusandrespitrecerra@gmail.com>
2026-09-17 19:00:21 -0500

    Update text and add navigation link in App component
    modifique el codigo

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,22 @@
+import { Link } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Asisencia</Text>
+      <Text style={styles.text}>Jesus Pitre</Text>
+      <Link href="/about" style={styles.button}>
+        Go to About Us
+      </Link>
     </View>
   );
 }
 const styles = StyleSheet.create({
+   button: {
+    fontSize: 18,
+    color: "#fff",
+    backgroundColor: "#415252",
+  },
   container: {
     flex: 1,
     justifyContent: "center",
@@ -20,3 +29,4 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
   },
 });
+
